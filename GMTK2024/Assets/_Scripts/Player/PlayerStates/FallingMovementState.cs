cdecafd93d0b1961143ddc11d72c82850f9fbe6d
Igor <igor.flores96@hotmail.com>
2024-08-19 21:57:55 -0300

    adicionado giro no falling state

--- GMTK2024/Assets/_Scripts/Player/PlayerStates/FallingMovementState.cs
+++ GMTK2024/Assets/_Scripts/Player/PlayerStates/FallingMovementState.cs
@@ -14,8 +14,15 @@ public class FallingMovementState : PlayerBaseState
 
     public override void UpdateState(PlayerMovement playerContext)
     {
-        playerContext.Rb.velocity = new Vector2(playerContext.Rb.velocity.x, -0.1f * playerContext.Speed);
         playerContext.UpdateRayCast();
+        playerContext.Rb.velocity = new Vector2(playerContext.Rb.velocity.x, -0.1f * playerContext.Speed);
+
+        float boostX = playerContext.PlayerActions.Movement.Boost.ReadValue<Vector2>().x;
+        float boostY = playerContext.PlayerActions.Movement.Boost.ReadValue<Vector2>().y;
+
+        float rotation = boostX + boostY * playerContext.RotationSpeed;
+        playerContext.Rb.MoveRotation(playerContext.Rb.rotation + rotation * Time.fixedDeltaTime);
+
     }
 
 }
